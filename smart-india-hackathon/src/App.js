93f74636d2ff26f0ed80066782bf9092159d9770
ananya142k <139583841+ananya142k@users.noreply.github.com>
2023-09-24 00:32:05 +0530

    seller page 1st update

--- smart-india-hackathon/src/App.js
+++ smart-india-hackathon/src/App.js
@@ -1,13 +1,12 @@
 import './App.css';
 import { LandingPage } from './components/LandingPage';
+import LoginSignup from './components/login-signup/LoginSignup';
 
 function App() {
   return (
-    <>
-    {/* // You need to tag your components here */}
-    <LandingPage></LandingPage>
-    {/* <ShippingWebsite></ShippingWebsite> */}
-    </>
+    <div>
+      <LoginSignup/>
+    </div>
   );
 }
 
