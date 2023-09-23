c119d9b18dca30287a907cff1062834f0a198ee8
Himanshu Chandna <himanshuchandna0007@gmail.com>
2023-09-23 19:59:07 +0530

    shipping sign in page created

--- smart-india-hackathon/src/App.js
+++ smart-india-hackathon/src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import { LandingPage } from './components/LandingPage';
+import { ShippingLogin } from './components/ShippingLogin';
 
 function App() {
   return (
     <>
     {/* // You need to tag your components here */}
     <LandingPage></LandingPage>
-    {/* <ShippingWebsite></ShippingWebsite> */}
+    <ShippingLogin></ShippingLogin>
     </>
   );
 }
