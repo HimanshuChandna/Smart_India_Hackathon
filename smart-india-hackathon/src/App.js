32487d64880c66a01503aa6d15f2d3cd06f191c2
Himanshu Chandna <himanshuchandna0007@gmail.com>
2023-09-23 12:19:39 +0530

    shipping website created

--- smart-india-hackathon/src/App.js
+++ smart-india-hackathon/src/App.js
@@ -1,24 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import { Shipping_website } from './components/Shipping_website';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    {/* // You need to tag your components here */}
+    <Shipping_website></Shipping_website>
+    </>
   );
 }
 
---
+++ smart-india-hackathon/src/components/Shipping_website.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Shipping_website = () => {
+  return (
+    <div>
+
+    <h1>This is the shipping website.</h1>
+
+    </div>
+  )
+}
