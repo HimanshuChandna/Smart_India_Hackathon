f08a3ea6bf299768bd1c141006d783f2267c1c11
ananya142k <139583841+ananya142k@users.noreply.github.com>
2023-09-24 10:37:39 +0530

    seller page 2nd update

--- smart-india-hackathon/src/components/login-signup/LoginSignup.jsx
+++ smart-india-hackathon/src/components/login-signup/LoginSignup.jsx
@@ -1,20 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './LoginSignup.css'
 
-
+import 
 
 const LoginSignup = () => {
+
+  const [action,setAction] = useState("Sign Up");
+
   return (
     <div className='container'>
       <div className="header">
-        <div className="text">Sign Up</div>
+        <div className="text">{action}</div>
         <div className="underline"></div>
       </div>
-      <div className="inputs"></div>
-        <div className="input">
-          <img src={user_icon} alt=""/>
-          <input type="text" placeholder='Name'/>
-        </div>
+      <div className="inputs">
+        {action=="Login"?<div></div>: <div className="input">
+        <img src={user_icon} alt=""/>
+        <input type="text" placeholder='Name'/>
+      </div>} 
+       
         <div className="input">
           <img src={email_icon} alt=""/>
           <input type="email" placeholder='Email Id'/>
@@ -23,11 +27,12 @@ const LoginSignup = () => {
           <img src={password_icon} alt=""/>
           <input type="password" placeholder='Password'/>
         </div>
-    
-      <div className="forget-password">Forget Password? <span>Click Here!</span></div>
+      </div>
+      {action=="Sign Up"? <div></div>:<div className="forget-password">Forget Password? <span>Click Here!</span></div>}
+      
       <div className="submit-container">
-        <div className="submit">Sign Up</div>
-        <div className="submit">Login</div>
+        <div className={action=="Login"?"submit gray":"submit"} onClick={()=>{setAction("Sign Up")}}>Sign Up</div>
+        <div className={action=="Sign Up"?"submit gray":"submit"} onClick={()=>{setAction("Login")}}>Login</div>
       </div> 
     </div>
   )
